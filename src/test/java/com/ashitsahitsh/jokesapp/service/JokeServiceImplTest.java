befaef78209dd18ec11cadc6f527bf872f64832d
asathish <ashits1991@gmail.com>
2020-05-03 23:57:08 -0400

    adding junit5 test case of the service class onto GIT.

---
+++ src/test/java/com/ashitsahitsh/jokesapp/service/JokeServiceImplTest.java
@@ -0,0 +1,21 @@
+package com.ashitsahitsh.jokesapp.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JokeServiceImplTest {
+
+    JokeService jokeService;
+
+    @BeforeEach
+    void setUp() {
+        jokeService = new JokeServiceImpl();
+    }
+
+    @Test
+    void getJoke() {
+        assertNotNull(jokeService.getJoke());
+    }
+}
