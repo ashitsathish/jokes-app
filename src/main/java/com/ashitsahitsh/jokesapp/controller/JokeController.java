59bf901790c29fe1a723ebd72bdb11249677b02a
asathish <ashits1991@gmail.com>
2020-05-03 23:52:38 -0400

    code changes with thymeleaf template and controller class to display joke on a html page. Also added a new banner.

---
+++ src/main/java/com/ashitsahitsh/jokesapp/controller/JokeController.java
@@ -0,0 +1,19 @@
+package com.ashitsahitsh.jokesapp.controller;
+
+import com.ashitsahitsh.jokesapp.service.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JokeController {
+    @Autowired
+    JokeService jokeService;
+
+    @GetMapping("/")
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+        return "chucknorris";
+    }
+}
